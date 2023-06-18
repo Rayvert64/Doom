00fad2d67243fc6b599e3e60ac9396a7f56ed139
Adora Boutin <philippe.boutin@protonmail.com>
2023-06-18 16:38:44 -0400

    Navigation shortcut rework

--- keymaps.el
+++ keymaps.el
@@ -1,4 +1,17 @@
 ;;; keymaps.el -*- lexical-binding: t; -*-
 ;; This is where I am putting my keybindings.
-(map! :nv "C-h" #'evil-backward-WORD-begin)
-(map! :nv "C-l" #'evil-forward-WORD-end)
+
+;;; Movement in all modes
+(map! :nvi "C-h" #'evil-backward-WORD-begin)
+(map! :nvi "C-l" #'evil-forward-WORD-end)
+;; Movement for both normal in visual mode
+(map! :nv "C-j" #'evil-forward-paragraph)
+(map! :nv "C-k" #'evil-backward-paragraph)
+;; Movement for normal mode only
+(map! :n "C-M-l" #'evil-window-right)
+(map! :n "C-M-h" #'evil-window-left)
+(map! :n "C-M-j" #'evil-window-down)
+(map! :n "C-M-k" #'evil-window-up)
+;; Movement for insert mode
+(map! :i "C-j" #'evil-next-line)
+(map! :i "C-k" #'evil-previous-line)
